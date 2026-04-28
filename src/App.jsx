df17c3f0cf4e3ffacf475dda704151da49775a84
creatly-space <Ludvig@creatly.se>
2026-04-28 22:39:58 +0200

    Fix closing div tag in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ export default function App() {
                           fontFamily: 'system-ui',
                 }}>
                           Laddar...
-                </div>div>
+                </div>div></div>
               )
   }
 
